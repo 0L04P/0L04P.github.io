11199e0fd99001d1ea909bd43cefb201249cc912
0L04P <87896597+0L04P@users.noreply.github.com>
2022-01-08 17:38:10 +0100

    Add files via upload

---
+++ Errori.js
@@ -0,0 +1,170 @@
+$(document).ready(function(){
+	 //non serve
+	//distruggi();
+}());
+ 
+ var array
+function crea(){	  
+	let strHTML = ``;
+	array= ERRORI_COMUNI.split('ITEM');
+	for(let i=1; i<array.length; ++i){
+
+		strHTML = `<span class='errori hidden' id='hid_sp${i}'><b>${i})</b>  ` + array[i].replace('-', '')  + `<br/></span>`;
+		strHTML += `<span class="errori" style="font-style: normal;" id='sp${i}'><b>${i})</b>  ` + array[i].replace('-', '')  + `<br/></span>`;
+		try {
+		  $('#divErrori').append(strHTML);
+		} 
+		catch (error) {
+		  console.error('errore riga ' + i + '\n' + error);	  
+		}
+	}	
+	
+	//TogliSpaziWhitePre();
+}
+
+function distruggi(){
+	$('#divErrori').html('');
+}
+ 
+ 
+ //qui perchè nel ready non esisteva ancora il divErrori
+window.addEventListener('DOMContentLoaded', (event) => {
+    crea();
+	$('#txtCerca').focus();
+	//TogliSpaziWhitePre();
+});
+
+const  arrTrovati = [];
+function Ricerca(){	
+
+	if($('#txtCerca').val() == '' && $('.evidenziato').length == 0){		
+		crea();
+	}
+	else if($('#txtCerca').val() == '' && $('.evidenziato').length > 0){		
+		//window.location.reload(true);
+		distruggi();
+		crea();
+	}
+	else{
+		$('.errori').css('display','none').removeClass('cercato');
+		let EmptyArr = [];
+		arrTrovati.length = 0;	
+		arrTrovati.splice();
+		//alert('----  ' + arrTrovati.length)
+		
+		for(let i=1; i<array.length; ++i){		
+			let Regex = new RegExp($('#txtCerca').val(), 'g')		
+			if(Regex.test(array[i])){			
+			
+				$(`#sp${i}`).css('display','block').addClass('cercato');
+				arrTrovati.push(i);						
+				let str = array[i];
+				//splitto la stringa				
+				let aux_str = str.substring(/label>/.exec(str).index+6).trim()
+				let new_str = `<b>${i})</b> ` + aux_str.replaceAll($('#txtCerca').val(), '<b class="evidenziato">' + $('#txtCerca').val() +'</b>');
+				$(`#sp${i}`).html(new_str)					
+			}
+		}	
+	} 
+	TogliSpaziWhitePre();
+}
+
+function Ricerca2(){debugger;
+	if($('#txtCerca2').val()!=''){
+		$('#txtCerca').attr('disabled', true)
+	}
+	else{
+		$('#txtCerca').attr('disabled', false)
+	}
+	
+	for(let i=0; i<arrTrovati.length; ++i){
+		
+		let Regex = new RegExp($('#txtCerca2').val(), 'g')		
+		if(Regex.test(array[arrTrovati[i]])){	
+			let k = arrTrovati[i];		
+			$(`#sp${arrTrovati[i]}`).css('display','block');					
+			//NEW														
+			let str = array[arrTrovati[i]];
+			//splitto la stringa				
+			let aux_str = str.substring(/label>/.exec(str).index+6).trim()
+			let aux_str_postRicerca1 = aux_str.replaceAll($('#txtCerca').val(), '<b class="evidenziato">' + $('#txtCerca').val() +'</b>');
+			let new_str = `<b>${i})</b> ` + aux_str_postRicerca1.replaceAll($('#txtCerca2').val(), '<b class="evidenziato2">' + $('#txtCerca2').val() +'</b>');
+			$(`#sp${arrTrovati[i]}`).html(new_str);				
+		}
+		else{
+			$(`#sp${arrTrovati[i]}`).css('display','none');			
+		}
+	}
+	
+	$('body').css('opacity', 1);
+	TogliSpaziWhitePre();
+} 
+//filtro rapido js
+function btnjs(){
+	//$('.argomento').parent().css('display','none'); 
+	//$('.JS').parent().css('display','');
+	if($('#btnJS').css('color') != 'rgb(255, 255, 0)')
+	{
+		$('#btnJS').css('color','rgb(255, 255, 0)').css('font-size','20px');
+		$('#btnVB').css('color','rgb(255, 255, 255)').css('font-size','14px');
+		
+		$('.argomento').parent().css('display','none'); 
+		$('.JS').parent().css('display','');
+		TogliSpaziWhitePre();
+	}
+	else{
+		$('#btnJS').css('color','rgb(255, 255, 255)').css('font-size','14px');		
+		
+		Pulisci();
+	}	
+}
+//filtro rapido vb
+function btnvb(){
+	//$('.argomento').parent().css('display','none'); 
+	//$('.VB').parent().css('display','');
+		
+	if($('#btnVB').css('color') != 'rgb(255, 255, 0)')
+	{
+		$('#btnVB').css('color','rgb(255, 255, 0)').css('font-size','20px');
+		$('#btnJS').css('color','rgb(255, 255, 255)').css('font-size','14px');
+		
+		$('.argomento').parent().css('display','none'); 
+		$('.VB').parent().css('display','');
+		TogliSpaziWhitePre();
+	}
+	else{
+		$('#btnVB').css('color','rgb(255, 255, 255)').css('font-size','14px');
+
+		Pulisci();	
+	}
+	
+}
+
+function Pulisci(){
+	
+	$('#txtCerca').val('').focus().attr('disabled', false);
+	$('#txtCerca2').val('');
+	$('.argomento').parent().css('display','');
+	$('#btnVB').css('color','white').css('font-size','14px'); 
+	$('#btnJS').css('color','white').css('font-size','14px'); 
+	
+	Ricerca();	
+	TogliSpaziWhitePre();
+}
+
+function ToTop(velocita){
+	
+	$("html, body").animate({ scrollTop: 0 }, velocita); 
+	return false;
+}
+
+function TogliSpaziWhitePre(){	
+
+	let i = 1;
+	let id = `sp${i}`;
+	while($('#'+id).length > 0){	
+		$('#'+id).html($('#'+id).html().replace(/\<br\>$/, ''));
+		i+=1;
+		id = `sp${i}`;
+	}
+}
