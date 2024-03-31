25488c4694fd5c3f8960bdf29399accb242dde57
0L04P <87896597+0L04P@users.noreply.github.com>
2024-03-31 18:15:38 +0200

    Add files via upload

---
+++ Libreria.mjs
@@ -0,0 +1,19 @@
+export let myVariable = Math.sqrt(2);
+
+function funzione_1(){
+	alert(1)
+}
+
+function funzione_2(){
+	alert(2)
+}
+
+
+export function funzione_3(){
+	alert(3)
+}
+
+
+  function funzione_4(){
+	alert(4)
+}
