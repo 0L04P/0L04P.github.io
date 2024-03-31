cf2352f12d4728863026222dde3b22c83378e953
0L04P <87896597+0L04P@users.noreply.github.com>
2024-03-31 18:30:00 +0200

    Update Libreria.mjs

--- Libreria.mjs
+++ Libreria.mjs
@@ -1,19 +1,13 @@
 export let myVariable = Math.sqrt(2);
 
-export function funzione_1(){
-	alert(1)
+export function funzione_1() {
+    alert(1);
 }
 
-export function funzione_2(){
-	alert(2)
+export function funzione_2() {
+    alert(2);
 }
 
-
-export function funzione_3(){
-	alert(3)
-}
-
-
- export function funzione_4(){
-	alert(4)
+export function funzione_3() {
+    alert(3);
 }
