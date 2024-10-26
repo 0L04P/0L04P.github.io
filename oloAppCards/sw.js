72be809587b84110e1312fd5b101c80ceaf92a6e
0L04P <87896597+0L04P@users.noreply.github.com>
2024-10-26 18:17:30 +0200

    Update sw.js

--- oloAppCards/sw.js
+++ oloAppCards/sw.js
@@ -1,4 +1,3 @@
-
 const CACHE_NAME = `oloAppCards_CACHE`;
 
 // Use the install event to pre-cache all initial resources.
@@ -6,31 +5,41 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/'      	 
+      'conanSito.html'
     ]);
   })());
 });
 
-	self.addEventListener('fetch', event => {
-	  event.respondWith((async () => {
-		const cache = await caches.open(CACHE_NAME);
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
 
-		// Get the resource from the cache.
-		const cachedResponse = await cache.match(event.request);
-		if (cachedResponse) {
-		  return cachedResponse;
-		} else {
-			try {
-			  // If the resource was not in the cache, try the network.
-			  const fetchResponse = await fetch(event.request);
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
 
-			  // Save the resource in the cache and return it.
-			  cache.put(event.request, fetchResponse.clone());
-			  return fetchResponse;
-			} catch (e) {
-			  // The network failed.
-			}
-		}
-	  })());
-	});	
-	
+          // Save the resource in the cache and return it.
+         // cache.put(event.request, fetchResponse.clone());
+
+        if (
+            event.request.url.startsWith('chrome-extension') ||
+            event.request.url.includes('extension') ||
+            !(event.request.url.indexOf('http') === 0)
+        ) return;
+          //lo sposto qui
+        cache.put(event.request, fetchResponse.clone());
+          
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.                    
+          console.log('The network failed:');
+		      console.log(e);
+        }
+    }
+  })());
+});
