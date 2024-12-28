d4427e344c5a0bfdee72730fdf7e77119a91f976
0L04P <87896597+0L04P@users.noreply.github.com>
2024-12-28 19:37:32 +0100

    Update sw.js

--- Conan/sw.js
+++ Conan/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `conanSitoCache_v13`;
+const CACHE_NAME = `conanSitoCache_v14`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
