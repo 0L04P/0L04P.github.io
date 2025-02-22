487efb1d57f5a25996679e3ca116929e2fadcf7a
0L04P <87896597+0L04P@users.noreply.github.com>
2025-02-22 17:02:42 +0100

    Update sw.js

--- OnePiece/sw.js
+++ OnePiece/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `onePieceSitoCache_v15`;
+const CACHE_NAME = `onePieceSitoCache_v1`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
